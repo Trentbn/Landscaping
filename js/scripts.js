67ee304734f77acb2523894a82a89ec1760733f9
Trent Nelson <trentnelson97@gmail.com>
2025-11-07 16:35:55 -0700

    created landscaping site

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+console.log("hello");
+const btnElement = document.querySelector("#hamburgerBtn");
+const navElement = document.querySelector("#theNav");
+
+btnElement.addEventListener("click", () => {
+    btnElement.classList.toggle("open");
+    navElement.classList.toggle("open");
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: "slide",
+    speed: 2000,
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // start autoplay
+    autoplay: {
+        delay: 5000,
+    },
+  });
